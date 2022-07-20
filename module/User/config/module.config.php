12dd9ad128d90577147271e622c2bb244f7ceb9b
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2022-07-20 09:55:42 +0430

    Specifying User Module Entity Locations

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -2,12 +2,28 @@
 
 namespace User;
 
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+
 return [
     'router' => [
         'routes' => [
         ],
     ],
     'controllers' => [
+    ],
+    'doctrine' => [
+        'driver' => [
+            __NAMESPACE__ . '_driver' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [__DIR__ . '/../src/Model/Entity']
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    __NAMESPACE__ . '\Model\Entity' => __NAMESPACE__ . '_driver'
+                ]
+            ]
+        ]
     ],
     'view_manager' => [
         'template_path_stack' => [
